86aad379bb60fb9bc41fa399f9c9f4fe72cc4f28
aacismaharjan <aashishmaharjan2001@gmail.com>
2021-08-10 12:32:41 +0545

    [Feat] Front Page UI

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Jumbotron from "./components/jumbotron";
+import jumboData from "./fixtures/jumbo.json";
+
+export default function App() {
+  return (
+    <Jumbotron.Container>
+      {jumboData &&
+        jumboData.length > 0 &&
+        jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+
+            <Jumbotron.Pane>
+              <Jumbotron.Image
+                src={item.image}
+                alt={item.alt}
+              ></Jumbotron.Image>
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+    </Jumbotron.Container>
+  );
+}
